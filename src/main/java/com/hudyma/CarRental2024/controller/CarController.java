8cc2ea4f7727e85f8100f4e861fcfcea176a12a7
yurkohudyma <hudyma@gmail.com>
2024-02-08 10:53:43 +0200

    Fixed UI and BE bugs on stats block display

--- src/main/java/com/hudyma/CarRental2024/controller/CarController.java
+++ src/main/java/com/hudyma/CarRental2024/controller/CarController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Log4j2
 @RequestMapping("/cars")
